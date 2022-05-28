4dac9663c95859319da74a6e6a8f9f0159bb0c1c
bsoalio <bsoalio@gmail.com>
2022-05-28 11:06:02 +0000

    Clean the code

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function computerPlay() {
   let selections = ["rock", "paper", "scissors"];
   let randomIndex = Math.floor(Math.random() * selections.length);
   let selection = selections[randomIndex];
-  //   console.log(selection);
+
   return selection;
 }
 
@@ -20,7 +20,6 @@ function playRound(playerSelection, computerSelection) {
   let winner;
   // if player selection and computer selection match, it is a draw
   if (playerSelection === computerSelection) {
-    // console.log("it is a draw");
     winner = "";
   }
   // the possible wins for the player
@@ -29,12 +28,8 @@ function playRound(playerSelection, computerSelection) {
     (playerSelection == "scissors" && computerSelection == "paper") ||
     (playerSelection == "paper" && computerSelection == "rock")
   ) {
-    // console.log("You win the round ");
     winner = "player";
-  }
-  // else if player selection is scissors
-  else {
-    // console.log("You lose the round ");
+  } else {
     winner = "computer";
   }
   return winner;
@@ -44,9 +39,6 @@ function playRound(playerSelection, computerSelection) {
 let computerScore = 0;
 let playerScore = 0;
 
-//
-
-// do {
 buttons.forEach((button) =>
   button.addEventListener("click", function () {
     if (computerScore === 5 || playerScore === 5) return; //stop the game
@@ -55,76 +47,25 @@ buttons.forEach((button) =>
     let winner = playRound(playerSelection, computerSelection);
     // Reset the result display
     roundWinner.textContent = "";
-    //update the score the winner
+    //update the score the winner and Print the winner of the round and give the score
     if (winner === "player") {
       ++playerScore;
-
       roundWinner.textContent = `${winner} wins the round`;
     } else if (winner === "computer") {
       ++computerScore;
-
       roundWinner.textContent = `${winner} wins the round`;
+    } else if (winner === "") {
+      roundWinner.textContent = "it is a draw";
     }
     //show the score on the page
     playerBoxScore.textContent = `${playerScore}`;
     computerBoxScore.textContent = `${computerScore}`;
-    //Print the winner of the round and give the score
-    if (winner === "") {
-      roundWinner.textContent = "it is a draw";
-      console.log(
-        `it is a draw, player score: ${playerScore} VS computer: ${computerScore}`
-      );
-    } else {
-      console.log(
-        `${winner} wins the round, player score: ${playerScore} VS computer: ${computerScore}`
-      );
-    }
 
     //print the winner of the game, the first to reach a score of 5
     if (playerScore === 5) {
       gameWinner.textContent = `Player wins the game`;
-      console.log(
-        `Player wins the game, player score: ${playerScore} VS computer: ${computerScore}`
-      );
     } else if (computerScore === 5) {
       gameWinner.textContent = `Computer wins the game`;
-      console.log(
-        `Computer wins the game, player score: ${playerScore} VS computer: ${computerScore}`
-      );
     }
   })
 );
-// } while (playerScore < 5 || computerScore < 5);
-// //Function to play the game x time
-// function game() {
-//   // track the computer and the player score
-//   let computerScore = 0;
-//   let playerScore = 0;
-//   //Loop through the number of round to play
-//   for (let i = 0; i < 5; i++) {
-//     //Player selection
-//     const playerSelection = prompt("enter your choice: ");
-//     //Computer selection
-//     const computerSelection = computerPlay();
-
-//     //find winner of the round
-//     let winner = playRound(playerSelection, computerSelection);
-//     console.log(winner);
-
-//     //update the score the winner
-//     if (winner === "player") {
-//       ++playerScore;
-//     } else if (winner === "computer") {
-//       ++computerScore;
-//     }
-//   }
-
-//   // print the winner of the game
-//   if (playerScore > computerScore) {
-//     alert(`player won. player: ${playerScore}, computer: ${computerScore}`);
-//   } else {
-//     alert(`computer won. player: ${playerScore}, computer: ${computerScore}`);
-//   }
-// }
-
-// game();
